5739eec2993e6f85545321c73007607328487e81
tdillon <tdillon@users.noreply.github.com>
2016-08-08 22:47:00 -0400

    worked on rpsls

--- app/github.ts
+++ app/github.ts
@@ -1,3 +1,7 @@
 fetch('https://api.github.com/users/tdillon/repos')
     .then(r => r.json())
     .then(d => document.querySelector('#numGithubRepos').textContent = d.length);
+
+
+//https://api.github.com/repos/tdillon/tdillon.github.io/stats/contributors
+//get for each repo to get # of commits, additions, and deletions
--- app/rpslp.ts
+++ app/rpslp.ts
@@ -6,19 +6,37 @@ import 'rxjs/add/operator/do'
 let rpslsP1Score = <HTMLSpanElement>document.querySelector('#rpslsP1Score');
 let rpslsP2Score = <HTMLSpanElement>document.querySelector('#rpslsP2Score');
 
+let rpslsP1 = <HTMLSpanElement>document.querySelector('#rpslsP1');
+let rpslsP2 = <HTMLSpanElement>document.querySelector('#rpslsP2');
+
 let p1Score = 0;
 let p2Score = 0;
 
 let moves = ['fa-hand-rock-o', 'fa-hand-paper-o', 'fa-hand-scissors-o', 'fa-hand-lizard-o', 'fa-hand-spock-o']
+let outcomeClasses = ['win', 'loose', 'draw'];
 
-let p1 = <HTMLElement>document.querySelector('#rpslpP1Move');
-let p2 = <HTMLElement>document.querySelector('#rpslpP2Move');
+let p1 = <HTMLElement>document.querySelector('#rpslsP1Move');
+let p2 = <HTMLElement>document.querySelector('#rpslsP2Move');
 
-Observable.interval(4000)
+let [r1, r2, r3] = <NodeListOf<HTMLDivElement>>document.querySelectorAll('#rpsls > div');
+
+Observable.interval(7000)
     .do(x => {
-        p1.style.color = p2.style.color = 'white';
-        p1.className = p2.className = `fa ${moves[0]}`;
+        r1.classList.add('current');
+        r2.classList.remove('current');
+        r3.classList.remove('current');
+        p1.classList.remove('win', 'loose', 'draw');
+        p2.classList.remove('win', 'loose', 'draw');
+        rpslsP1.classList.remove('win', 'loose', 'draw');
+        rpslsP2.classList.remove('win', 'loose', 'draw');
     })
+    .delay(1500)
+    .do(x => {
+        r1.classList.remove('current');
+        r2.classList.add('current');
+    })
+    .delay(250)
+    .do(x => p1.className = p2.className = `fa ${moves[0]}`)
     .delay(250)
     .do(x => p1.className = p2.className = `fa ${moves[1]}`)
     .delay(250)
@@ -31,30 +49,44 @@ Observable.interval(4000)
     .do(x => {
         p1.className = `fa ${moves[Math.floor(Math.random() * moves.length)]}`;
         p2.className = `fa ${moves[Math.floor(Math.random() * moves.length)]}`;
-    })
-    .delay(250)
-    .do(x => {
+
         let winner = Math.floor(Math.random() * 3);
 
         switch (winner) {
             case 0://tie
-                p1.style.color = p2.style.color = '#09c';
+                p1.classList.add('draw');
+                p2.classList.add('draw');
+                rpslsP1.classList.add('draw');
+                rpslsP2.classList.add('draw');
                 break;
             case 1://p1 win
-                p1.style.color = '#0f0';
-                p2.style.color = '#f00';
+                p1.classList.add('win');
+                p2.classList.add('loose');
+                rpslsP1.classList.add('win');
+                rpslsP2.classList.add('loose');
                 ++p1Score;
                 break;
             case 2://p2 win
-                p1.style.color = '#f00';
-                p2.style.color = '#0f0';
+                p1.classList.add('loose');
+                p2.classList.add('win');
+                rpslsP1.classList.add('loose');
+                rpslsP2.classList.add('win');
                 ++p2Score;
                 break;
             default:
                 break;
         }
-
+    })
+    .delay(1500)
+    .do(x => {
+        r2.classList.remove('current');
+        r3.classList.add('current');
+    })
+    .delay(1000)
+    .do(x => {
         rpslsP1Score.textContent = p1Score.toString();
         rpslsP2Score.textContent = p2Score.toString();
     })
+    .delay(750)
+    .do(x => r3.classList.remove('current'))
     .subscribe();
